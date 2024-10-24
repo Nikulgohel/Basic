79e4481c509d4904a93818f4dc8addb94154964a
Nikulgohel <nikulgohel1611@gmail.com>
2024-10-24 22:37:39 +0530

    Add project files.

---
+++ basic/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+//int a = 2;
+//int b = 3;
+
+//a=a +  b;//a=5
+//b=a - b;//b=2
+//a=a - b;//a=3
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+
+
+
+//int c = 12;
+//int d = 3;
+
+//c = c / d;//c=4
+//d = c * d;//d=12
+//c = d / c;//c=3
+
+//Console.WriteLine(c);
+//Console.WriteLine(d);
+
+
+//int side = 5;
+
+//a = side * side; //area of square
+
+//Console.WriteLine(a);
+
+
+//int height = 4;
+//int tbase = 3;
+
+//a = tbase * height/2;// area of triangle
+
+//Console.WriteLine(a);
+
+//char h = 'B';
+
+//char c = Convert.ToChar(h + 32);
+//Console.WriteLine(c);
+
+
+//char c = Convert.ToChar(h + 32);
+
+
+char ch = 'G';
+
+if (ch >= 'A' && ch <= 'Z')
+{
+    // Convert the character to lowercase by adding 32
+    ch = (char)(ch + 32);
+}
+
+Console.WriteLine(ch); // Output will be 'b'
+
